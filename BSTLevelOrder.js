e92c7daf88d7b78e015461e6f8beb25d1e64a89b
In Hyub <bruinhyub@gmail.com>
2019-02-02 17:55:43 -0800

    added iterative solution

--- BSTLevelOrder.js
+++ BSTLevelOrder.js
@@ -34,3 +34,29 @@ var levelOrder = function(root) {
 };
 
 // using recursion, try iterative
+
+var levelOrder = function(root) {
+    if (!root) return [];
+    let result = [[root.val]];
+    let nodeArr = [root];
+    while (nodeArr.length) {
+        let nodeArr2 = [];
+        let values = [];
+        for (let i = 0; i<nodeArr.length; i++) {
+            if(nodeArr[i].left) {
+                nodeArr2.push(nodeArr[i].left);
+                values.push(nodeArr[i].left.val);
+            }
+             if(nodeArr[i].right) {
+                nodeArr2.push(nodeArr[i].right);
+                values.push(nodeArr[i].right.val);
+            }
+        }
+        if (!values.length) break;
+        result.push(values);
+        nodeArr = nodeArr2;
+    }
+    return result;
+};
+
+// iterative, most optimal solution, faster than 100% of submissions
