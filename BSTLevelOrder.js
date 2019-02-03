acdc5f990609aa8f98fafe27c95aff59cf3fa7f2
In Hyub <bruinhyub@gmail.com>
2019-02-02 17:45:32 -0800

    returns values of nodes in BST in level order

---
+++ BSTLevelOrder.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (!root) return [];
+    let result = [[root.val]];
+    let nodeArr = [root];
+    const recurse = (nodeArr2, values) => {
+        for (let i = 0; i<nodeArr.length; i++) {
+            if (nodeArr[i].left) {
+                nodeArr2.push(nodeArr[i].left);
+                values.push(nodeArr[i].left.val);
+            }
+            if (nodeArr[i].right) {
+                nodeArr2.push(nodeArr[i].right);
+                values.push(nodeArr[i].right.val);
+            }
+        }
+        if (values.length === 0) return;
+        result.push(values);
+        nodeArr = nodeArr2;
+        if(nodeArr.length > 0) recurse([],[]);
+    }
+    recurse([],[]);
+    return result;
+};
+
+// using recursion, try iterative
