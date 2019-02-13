5f266d0bcc76bd796d20a269c36a6a8eae322fd5
In Hyub <bruinhyub@gmail.com>
2019-02-12 16:10:49 -0800

    added dynamic programming solution

--- numOfArithmeticSlices.js
+++ numOfArithmeticSlices.js
@@ -15,3 +15,20 @@ var numberOfArithmeticSlices = function(A) {
     }
     return num;
 };
+
+// brute force O(n^2)
+
+var numberOfArithmeticSlices = function(A) {
+    let num = 0;
+    let dp = Array(A.length).fill(0);
+    for (let i = 2; i < A.length; i++) {
+        if (A[i] - A[i - 1] === A[i - 1] - A[i - 2]) {
+            dp[i] = dp[i - 1] + 1;
+            num += dp[i];
+        }
+    }
+    return num;
+};
+
+// dynamic programming O(n)
+// can do constant space since you only need the last saved value for dp
